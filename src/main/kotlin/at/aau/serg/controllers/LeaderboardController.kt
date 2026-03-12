a18f73b5ad99bb3f564ac082c853b622acc80831
Adrian Pushkollaj <adrianpushkollaj@gmx.at>
2026-03-12 20:16:03 +0100

    rank parameter für leaderboard eingebaut, bei null weiterhhin ganzes leaderboard, tests passen noch

--- src/main/kotlin/at/aau/serg/controllers/LeaderboardController.kt
+++ src/main/kotlin/at/aau/serg/controllers/LeaderboardController.kt
@@ -5,6 +5,9 @@ import at.aau.serg.services.GameResultService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.http.HttpStatus
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("/leaderboard")
@@ -13,7 +16,19 @@ class LeaderboardController(
 ) {
 
     @GetMapping
-    fun getLeaderboard(): List<GameResult> =
-        gameResultService.getGameResults().sortedWith(compareBy ({ -it.score}, {it.timeInSeconds}))
+    fun getLeaderboard(@RequestParam(required = false) rank: Int? = null): List<GameResult> {
+        val leaderboard = gameResultService.getGameResults().sortedWith(compareBy({ -it.score }, { it.timeInSeconds }))
 
+        if (rank == null) { return leaderboard }
+
+        if (rank < 1 || rank > leaderboard.size) {
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST)
+        }
+
+        val index = rank - 1
+        val start = maxOf(0, index - 3)
+        val end = minOf(leaderboard.size, index + 4)
+
+        return leaderboard.subList(start, end)
+    }
 }
